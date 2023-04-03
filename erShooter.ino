f664677fa5c7373d4bb1e49ddd8c1255d2f8e1b4
BlackCommando2 <keshavsoni2002@gmail.com>
2023-04-03 10:39:22 +0530

    Update erShooter.ino

--- erShooter.ino
+++ erShooter.ino
@@ -7,9 +7,11 @@ Motor leftMotor(10, 8);
 Peer remote;
 JSONVar feedback;
 int currentPwm = 0,pwmChange = 10;
+int pneumaticPin = 32; 
 void setup()
 {
     Serial.begin(115200);
+    pinMode(pneumaticPin,OUTPUT);
     setId("EShoo");
     remote.init("TenZZ");
     remote.setOnRecieve(poleOne,"pOne");
@@ -18,6 +20,8 @@ void setup()
     remote.setOnRecieve(incPwm,"up");
     remote.setOnRecieve(decPwm,"down");
     remote.setOnRecieve(resetAll,"shRst");
+    remote.setOnRecieve(pneumaticOpen,"pOpen");
+    remote.setOnRecieve(pneumaticClose,"pClose");
 }
 
 void loop()
@@ -27,14 +31,22 @@ void loop()
 
 void incPwm(JSONVar msg)
 {
-    rightMotor.setPWM(currentPwm + pwmChange);
-    leftMotor.setPWM(currentPwm + pwmChange);
+    currentPwm = currentPwm + pwmChange;
+    currentPwm = currentPwm > 255 ? 255 : currentPwm;
+    rightMotor.setPWM(currentPwm);
+    leftMotor.setPWM(currentPwm);
+    Serial.println(JSON.stringify(msg));
+    Serial.println("CurrentPWM: " +String(currentPwm));
 }
 
 void decPwm(JSONVar msg)
 {
-    rightMotor.setPWM(currentPwm - pwmChange);
-    leftMotor.setPWM(currentPwm + pwmChange);
+    currentPwm = currentPwm - pwmChange;
+    currentPwm = currentPwm < 0 ? 0 : currentPwm;
+    rightMotor.setPWM(currentPwm);
+    leftMotor.setPWM(currentPwm);
+    Serial.println(JSON.stringify(msg));
+    Serial.println("CurrentPWM: " +String(currentPwm));
 }
 
 void poleOne(JSONVar msg)
@@ -42,18 +54,24 @@ void poleOne(JSONVar msg)
     currentPwm = 90;
     rightMotor.setPWM(currentPwm);
     leftMotor.setPWM(currentPwm);
+    Serial.println(JSON.stringify(msg));
+    Serial.println("CurrentPWM: " +String(currentPwm));
 }
 void poleTwo(JSONVar msg)
 {
     currentPwm = 180;
     rightMotor.setPWM(currentPwm);
     leftMotor.setPWM(currentPwm);
+    Serial.println(JSON.stringify(msg));
+    Serial.println("CurrentPWM: " +String(currentPwm));
 }
 void poleThree(JSONVar msg)
 {
     currentPwm = 255;
     rightMotor.setPWM(currentPwm);
     leftMotor.setPWM(currentPwm);
+    Serial.println(JSON.stringify(msg));
+    Serial.println("CurrentPWM: " +String(currentPwm));
 }
 
 void resetAll(JSONVar msg)
@@ -61,4 +79,18 @@ void resetAll(JSONVar msg)
     currentPwm = 0;
     rightMotor.setPWM(currentPwm);
     leftMotor.setPWM(currentPwm);
+    Serial.println(JSON.stringify(msg));
+    Serial.println("CurrentPWM: " +String(currentPwm));
+}
+
+void pneumaticOpen(JSONVar msg)
+{
+    digitalWrite(pneumaticPin,HIGH);
+    Serial.println(JSON.stringify(msg));
+}
+
+void pneumaticClose(JSONVar msg)
+{
+    digitalWrite(pneumaticPin,LOW);
+    Serial.println(JSON.stringify(msg));
 }
